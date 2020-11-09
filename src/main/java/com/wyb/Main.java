2099448612a1d849eddf98332e79b78e6f6054b9
wybzxx <1127810284@qq.com>
2020-11-09 23:51:40 +0800

    ....

---
+++ src/main/java/com/wyb/Main.java
@@ -0,0 +1,10 @@
+package com.wyb;
+
+/**
+ * @program: patterns
+ * @description:
+ * @author: wuyuebin
+ * @create: 2020-11-09 23:50
+ **/
+public class Main {
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,8 @@
+/**
+ * @program: patterns
+ * @description:
+ * @author: wuyuebin
+ * @create: 2020-11-09 23:51
+ **/
+public class Test {
+}
